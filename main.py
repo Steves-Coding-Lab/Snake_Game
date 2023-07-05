5e0d47c963e69abb9c42c84b0b4b47c28ce004e4
Mark <mark@email.com>
2023-07-05 22:34:36 +0100

    Coding_1

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ pygame.init()
 # Set the screen dimensions
 screen_width = 640
 screen_height = 480
+grid_size = 10
 
 # Set the colors
 black = pygame.Color(0, 0, 0)
@@ -14,18 +15,36 @@ white = pygame.Color(255, 255, 255)
 red = pygame.Color(255, 0, 0)
 green = pygame.Color(0, 255, 0)
 
-# Set the initial position and size of the snake
-snake_pos = [[100, 50], 
-             [90, 50], 
-             [80, 50]]
-snake_size = 10
-
 # Set the initial direction of the snake
 direction = "RIGHT"
 
-# Set the initial position of the food
-food_pos = [random.randrange(1, screen_width // 10) * 10,
-            random.randrange(1, screen_height // 10) * 10]
+# Set the initial position of the food or Snake
+def random_pos():
+    return [random.randrange(1, screen_width // grid_size) * grid_size,
+            random.randrange(1, screen_height // grid_size) * grid_size]
+
+def display_score(score):
+    score_text = font_style.render("Score: " + str(score), True, white)
+    screen.blit(score_text, [10, 10])
+
+def game_over():
+    message = font_style.render("Game Over", True, white)
+    screen.blit(message, [screen_width // 2 - 60, screen_height // 2 - 30])
+    pygame.display.flip()
+    pygame.time.wait(2000)
+    pygame.quit()
+
+
+
+# Set the initial position of the snake
+snake_pos = [[screen_width // 2, screen_height // 2]] 
+
+#Food Position and Snake position cannot be the same
+while True:
+    food_pos = random_pos()
+    if food_pos != snake_pos[0]:
+        break
+    
 food_spawned = True
 
 # Set the clock to control the frame rate
@@ -35,17 +54,6 @@ clock = pygame.time.Clock()
 font_style = pygame.font.SysFont(None, 30)
 
 
-def display_score(score):
-    score_text = font_style.render("Score: " + str(score), True, white)
-    screen.blit(score_text, [10, 10])
-
-
-def game_over():
-    message = font_style.render("Game Over", True, white)
-    screen.blit(message, [screen_width // 2 - 60, screen_height // 2 - 30])
-    pygame.display.flip()
-    pygame.time.wait(2000)
-    pygame.quit()
 
 
 # Create the game window
@@ -71,13 +79,13 @@ while not game_over_flag:
                 direction = "RIGHT"
 
     if direction == "UP":
-        snake_pos[0][1] -= snake_size
+        snake_pos[0][1] -= grid_size
     elif direction == "DOWN":
-        snake_pos[0][1] += snake_size
+        snake_pos[0][1] += grid_size
     elif direction == "LEFT":
-        snake_pos[0][0] -= snake_size
+        snake_pos[0][0] -= grid_size
     elif direction == "RIGHT":
-        snake_pos[0][0] += snake_size
+        snake_pos[0][0] += grid_size
 
     if snake_pos[0][0] < 0 or snake_pos[0][0] >= screen_width or \
             snake_pos[0][1] < 0 or snake_pos[0][1] >= screen_height:
@@ -93,22 +101,21 @@ while not game_over_flag:
     #    snake_pos.pop()
 
     if not food_spawned:
-        food_pos = [random.randrange(1, screen_width // 10) * 10,
-                    random.randrange(1, screen_height // 10) * 10]
+        food_pos = random_pos()
         food_spawned = True
 
     screen.fill(black)
 
     for pos in snake_pos:
-        pygame.draw.rect(screen, green, pygame.Rect(pos[0], pos[1], snake_size, snake_size))
+        pygame.draw.rect(screen, green, pygame.Rect(pos[0], pos[1], grid_size, grid_size))
 
-    pygame.draw.rect(screen, red, pygame.Rect(food_pos[0], food_pos[1], snake_size, snake_size))
+    pygame.draw.rect(screen, red, pygame.Rect(food_pos[0], food_pos[1], grid_size, grid_size))
 
     display_score(score)
 
     pygame.display.flip()
 
-    clock.tick(20)
+    clock.tick(5)
 
 # Quit the game
 pygame.quit()
